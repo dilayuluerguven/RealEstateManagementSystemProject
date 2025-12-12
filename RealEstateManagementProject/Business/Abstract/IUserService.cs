c60d4b52a4d54b84c677d53d887eea27a0ba412d
dilayuluerguven <uluerguven245@gmail.com>
2025-12-12 13:39:17 +0300

    Implemented user service and user service interface

---
+++ RealEstateManagementProject/Business/Abstract/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using RealEstateManagementProject.Dtos;
+
+namespace RealEstateManagementProject.Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<List<UserDTO>> GetAllUsersAsync();
+        Task<UserDTO> GetUserByIdAsync(int id);
+        Task<bool> CreateUserAsync(UserForRegisterDto dto);
+        Task<bool> UpdateUserAsync(int id, UserForRegisterDto dto);
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
---
+++ RealEstateManagementProject/Business/Concrete/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstateManagementProject.Business.Abstract;
+using RealEstateManagementProject.DataAccess;
+using RealEstateManagementProject.Dtos;
+using RealEstateManagementProject.Entities.Concrete;
+
+namespace RealEstateManagementProject.Business.Concrete
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<List<UserDTO>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<UserDTO> GetUserByIdAsync(int id) {
+            throw new NotImplementedException(); 
+        }
+        public Task<bool>CreateUserAsync(UserForRegisterDto dto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> UpdateUserAsync(int id, UserForRegisterDto dto)
+        {
+            throw new NotImplementedException(); 
+        }
+        public Task<bool> DeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
