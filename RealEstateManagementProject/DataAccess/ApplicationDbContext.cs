d09c596def12aed7100d7c15245d8e5a2127776a
dilayuluerguven <uluerguven245@gmail.com>
2025-12-12 13:38:57 +0300

    Created ApplicationDbContext and configured DbSets

---
+++ RealEstateManagementProject/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RealEstateManagementProject.Entities.Concrete;
+using RealEstateManagementProject.Entities;
+namespace RealEstateManagementProject.DataAccess
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        { 
+            
+        }
+        public DbSet<Il> Iller { get; set; }
+        public DbSet<Ilce> Ilceler { get; set; }
+        public DbSet<Mahalle> Mahalleler { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tasinmaz> Tasinmazlar { get; set; }
+        public DbSet<Log> Loglar { get; set; }
+
+    }
+
+}
