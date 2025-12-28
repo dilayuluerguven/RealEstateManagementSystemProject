2fbcfe45f6c6a92783d83fd1d446d1633be1a639
dilayuluerguven <uluerguven245@gmail.com>
2025-12-28 18:54:05 +0300

    Update property service API integration

--- realestatemanagementfrontend/src/app/core/tasinmaz/tasinmaz.service.ts
+++ realestatemanagementfrontend/src/app/core/tasinmaz/tasinmaz.service.ts
@@ -1,27 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Tasinmaz } from './models/tasinmaz';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasinmazService {
+  private apiUrl = `${environment.baseUrl}/api/Tasinmaz`;
+
   constructor(private httpClient: HttpClient) {}
-  getAll() {
-    return this.httpClient.get<any[]>(`${environment.baseUrl}/api/Tasinmaz`);
+
+  getAll(): Observable<Tasinmaz[]> {
+    return this.httpClient.get<Tasinmaz[]>(this.apiUrl);
   }
 
-  add(data: any) {
-    return this.httpClient.post(
-      `${environment.baseUrl}/api/Tasinmaz/add`,
-      data
-    );
+  getById(id: number): Observable<Tasinmaz> {
+    return this.httpClient.get<Tasinmaz>(`${this.apiUrl}/${id}`);
   }
 
-  update(id: number, data: any) {
-    return this.httpClient.put(
-      `${environment.baseUrl}/api/Tasinmaz/update/${id}`,
-      data
-    );
+  add(data: Tasinmaz): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/add`, data);
+  }
+
+  update(id: number, data: Tasinmaz): Observable<any> {
+    return this.httpClient.put(`${this.apiUrl}/update/${id}`, data);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/delete/${id}`);
   }
 }
+
