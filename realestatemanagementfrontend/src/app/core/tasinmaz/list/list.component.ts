4548a9da97c0a817d28bcf90074983f4adf9b991
dilayuluerguven <uluerguven245@gmail.com>
2025-12-28 18:53:06 +0300

    Update property listing screen

--- realestatemanagementfrontend/src/app/core/tasinmaz/list/list.component.ts
+++ realestatemanagementfrontend/src/app/core/tasinmaz/list/list.component.ts
@@ -1,24 +1,22 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { TasinmazService } from '../tasinmaz.service';
+import { Tasinmaz } from '../models/tasinmaz';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit{
-  tasinmazlar:any[]=[];
-  constructor(private httpClient:HttpClient,private tasinmazService:TasinmazService){
+export class ListComponent implements OnInit {
 
-  }
+  tasinmazlar: Tasinmaz[] = [];
+
+  constructor(private tasinmazService: TasinmazService) {}
+
   ngOnInit(): void {
-       this.tasinmazService.getAll().subscribe(res=>{
-        console.log('res:',res);
-        
-      this.tasinmazlar=res;
-    })
+    this.tasinmazService.getAll().subscribe(res => {
+      console.log('res:', res);
+      this.tasinmazlar = res;
+    });
   }
-  
-
 }
