0c544dd409d955932747b054e3d1cdf67be49e04
dilayuluerguven <uluerguven245@gmail.com>
2025-12-28 18:51:56 +0300

    Refactor authentication interceptor structure

--- realestatemanagementfrontend/src/app/auth/login/login.component.ts
+++ realestatemanagementfrontend/src/app/auth/login/login.component.ts
@@ -6,35 +6,36 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
-  formGroup=new FormGroup({
-    email:new FormControl('',[Validators.required,Validators.email]),
-    password:new FormControl('',[Validators.required])
-  })
- 
+export class LoginComponent implements OnInit {
+  formGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor(private authService:AuthService,private router:Router){
- 
-  }
-  ngOnInit():void{
-
-  }
-  show()
-  {
-    if(this.formGroup.invalid)
-    {
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit(): void {}
+  show() {
+    if (this.formGroup.invalid) {
       return;
     }
-    const email = this.formGroup.value.email ;
+    const email = this.formGroup.value.email;
     const password = this.formGroup.value.password;
-    this.authService.login(email as string,password as string).subscribe(x=>{
-      localStorage.setItem('token',x.token);
-      console.log(x);
-      this.router.navigate(['/core/tasinmaz/list']);
+    this.authService
+      .login(email as string, password as string)
+      .subscribe((x) => {
+        localStorage.setItem('token', x.token);
+        localStorage.setItem('role', x.rol);
+        localStorage.setItem('userId', x.id.toString());
+        console.log(x);
+        
 
-    })
+        if (x.rol === 'Admin') {
+          this.router.navigate(['/core/admin']);
+        } else {
+          this.router.navigate(['/core/tasinmaz/list']);
+        }
+      });
   }
-
 }
