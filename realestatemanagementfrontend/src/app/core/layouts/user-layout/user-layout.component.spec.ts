c7f824469dda7e84fa30aaa595540c485969ea3b
dilayuluerguven <uluerguven245@gmail.com>
2025-12-28 18:52:48 +0300

    Remove header component and introduce layout structure

--- realestatemanagementfrontend/src/app/core/layouts/user-layout/user-layout.component.spec.ts
+++ realestatemanagementfrontend/src/app/core/layouts/user-layout/user-layout.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { UserLayoutComponent } from './user-layout.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('UserLayoutComponent', () => {
+  let component: UserLayoutComponent;
+  let fixture: ComponentFixture<UserLayoutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ UserLayoutComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(UserLayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
