8ec4f9417a3a84fb187914f887ea281c90204e2b
dilayuluerguven <uluerguven245@gmail.com>
2025-12-28 18:59:00 +0300

    Add logout functionality to user layout component

--- realestatemanagementfrontend/src/app/core/layouts/user-layout/user-layout.component.ts
+++ realestatemanagementfrontend/src/app/core/layouts/user-layout/user-layout.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-layout',
   templateUrl: './user-layout.component.html',
   styleUrls: ['./user-layout.component.css']
 })
-export class UserLayoutComponent {
+export class UserLayoutComponent implements OnInit {
+  constructor(private router:Router) {
+    
+  }
+  ngOnInit(): void {
+      
+  }
+   logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+  }
 
 }
