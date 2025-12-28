656033a75cb57cac19ee8acfc82f2be452fe6bcc
dilayuluerguven <uluerguven245@gmail.com>
2025-12-28 18:54:21 +0300

    Add property-related model definitions

---
+++ realestatemanagementfrontend/src/app/core/tasinmaz/models/tasinmaz.ts
@@ -0,0 +1,12 @@
+export interface Tasinmaz {
+  id?: number;
+  userId:number;
+  ilId: number;
+  ilceId: number;
+  mahalleId: number;
+  ada: number;
+  parsel: number;
+  adres: string;
+  emlakTipi: string;
+  koordinat: string;
+}
