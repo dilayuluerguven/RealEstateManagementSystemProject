9de5c01090b11c5685d7a381b172defb9ae2b61f
dilayuluerguven <uluerguven245@gmail.com>
2025-12-31 15:10:53 +0300

    Updating auth module structure and imports

--- realestatemanagementfrontend/src/app/auth/auth.module.ts
+++ realestatemanagementfrontend/src/app/auth/auth.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
+    LoginComponent,
     RegisterComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
