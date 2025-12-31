a5e85670403ec2a289ad69eda91c596e0a7d81bd
dilayuluerguven <uluerguven245@gmail.com>
2025-12-31 15:11:02 +0300

    Improving authentication service login and register handling

--- realestatemanagementfrontend/src/app/auth/auth.service.ts
+++ realestatemanagementfrontend/src/app/auth/auth.service.ts
@@ -1,22 +1,43 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {environment} from 'src/environments/environment'
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient:HttpClient) { }
-  login(email: string, password: string):Observable<any>
-  {
-    const body={
-      email:email,
-      sifre:password
+  login(email: string, password: string): Observable<any> {
+    const body = {
+      email: email,
+      sifre: password,
     };
-    return this.httpClient.post<any>(`${environment.baseUrl}/api/Auth/login`, body);
+
+    return this.httpClient.post<any>(
+      `${environment.baseUrl}/api/Auth/login`,
+      body
+    );
+  }
+
+  register(
+    adSoyad: string,
+    email: string,
+    password: string,
+    rol: string
+  ): Observable<any> {
+    const body = {
+      adSoyad: adSoyad,
+      email: email,
+      sifre: password,
+      rol: rol,
+    };
+
+    return this.httpClient.post(
+      `${environment.baseUrl}/api/Auth/register`,
+      body,
+      { responseType: 'text' }
+    );
   }
 }
-
