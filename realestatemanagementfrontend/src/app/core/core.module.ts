a399bc28a7b410a06b03c11c173b7fd6722ad112
dilayuluerguven <uluerguven245@gmail.com>
2026-01-14 23:00:27 +0300

    register profile component and shared modules in core module

--- realestatemanagementfrontend/src/app/core/core.module.ts
+++ realestatemanagementfrontend/src/app/core/core.module.ts
@@ -4,15 +4,15 @@ import { CoreRoutingModule } from './core-routing.module';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
 import { RouterModule } from '@angular/router';
-import { TasinmazMapComponent } from './map/tasinmaz-map/tasinmaz-map.component';
-import { TasinmazListMapComponent } from './map/tasinmaz-list-map/tasinmaz-list-map.component';
+import { TasinmazMapComponent } from '../shared/tasinmaz-map/tasinmaz-map.component';
+import { TasinmazListMapComponent } from '../shared/tasinmaz-list-map/tasinmaz-list-map.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, UserLayoutComponent, TasinmazMapComponent, TasinmazListMapComponent, ProfileComponent],
-  imports: [CommonModule, CoreRoutingModule, RouterModule,HttpClientModule,FormsModule],
-  exports:[TasinmazMapComponent,TasinmazListMapComponent]
+  declarations: [AdminLayoutComponent, UserLayoutComponent,ProfileComponent],
+  imports: [CommonModule, CoreRoutingModule, RouterModule,HttpClientModule,FormsModule,ReactiveFormsModule],
+  
 })
 export class CoreModule {}
