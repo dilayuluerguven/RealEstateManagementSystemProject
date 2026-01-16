c2f77789cf406af21a76a19e67a0949f5d6ab82f
dilayuluerguven <uluerguven245@gmail.com>
2026-01-16 12:00:55 +0300

    Refactored area analysis service to use configurable API base URL

--- realestatemanagementfrontend/src/app/core/alan-analizi/services/alan-analizi.service.ts
+++ realestatemanagementfrontend/src/app/core/alan-analizi/services/alan-analizi.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlanAnaliziService {
 
-  constructor() { }
+  private apiUrl = `${environment.baseUrl}/api/AlanAnaliz`;
+
+  constructor(private http: HttpClient) {}
+
+  geometriKaydet(kullaniciId: number, body: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/geometri-kaydet?kullaniciId=${kullaniciId}`, body);
+  }
+
+  kayitliGeometriler(kullaniciId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/geometriler?kullaniciId=${kullaniciId}`);
+  }
+
+  kesisim(kullaniciId: number, body: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/kesisim?kullaniciId=${kullaniciId}`, body);
+  }
+
+  birlesim(kullaniciId: number, body: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/birlesim?kullaniciId=${kullaniciId}`, body);
+  }
+
+  tumAnalizler(kullaniciId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/tum-analizler?kullaniciId=${kullaniciId}`);
+  }
 }
