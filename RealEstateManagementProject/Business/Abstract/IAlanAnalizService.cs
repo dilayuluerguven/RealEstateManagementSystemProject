149e011a3914dc69aa5dc92122ebc8980bcb2ea8
dilayuluerguven <uluerguven245@gmail.com>
2026-01-16 11:25:40 +0300

    Defined service interface for area analysis operations

---
+++ RealEstateManagementProject/Business/Abstract/IAlanAnalizService.cs
@@ -0,0 +1,17 @@
+﻿using RealEstateManagementProject.Dtos;
+
+namespace RealEstateManagementProject.Business.Abstract
+{
+    public interface IAlanAnalizService
+    {
+        Task<bool> GeometriKaydetAsync(int kullaniciId, AlanAnalizCreateDto dto);
+
+        Task<List<AlanAnalizSonucDto>> KayitliGeometrileriGetirAsync(int kullaniciId);
+
+        Task<AlanAnalizSonucDto?> KesisimHesaplaAsync(int kullaniciId, AlanAnalizIslemDto dto);
+
+        Task<AlanAnalizSonucDto?> BirlesimHesaplaAsync(int kullaniciId, AlanAnalizIslemDto dto);
+
+        Task<List<AlanAnalizSonucDto>> TumAnalizleriGetirAsync(int kullaniciId);
+    }
+}
