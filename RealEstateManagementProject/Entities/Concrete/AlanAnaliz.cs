dbef4c6d576b9d535e475cc6b584b1bd04b39458
dilayuluerguven <uluerguven245@gmail.com>
2026-01-16 15:40:14 +0300

    Modify AlanAnaliz entity to include geometry fields and user reference fix

--- RealEstateManagementProject/Entities/Concrete/AlanAnaliz.cs
+++ RealEstateManagementProject/Entities/Concrete/AlanAnaliz.cs
@@ -1,32 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using RealEstateManagementProject.Entities.Concrete;
+﻿using System;
 
 namespace RealEstateManagementProject.Entities
 {
     public class AlanAnaliz
     {
-        [Key]
         public int Id { get; set; }
-
         public int KullaniciId { get; set; }
-
-        [ForeignKey(nameof(KullaniciId))]
-        public User Kullanici { get; set; }
-
-        [Required]
         public string GeometriAdi { get; set; }
-
-        [Required]
-        public string GeometriTuru { get; set; } 
-
-        public string? IslemTuru { get; set; }
-
-        [Required]
+        public string AnalizTuru { get; set; }
         public string GeometriJson { get; set; }
-
         public double AlanMetrekare { get; set; }
-
         public DateTime OlusturmaTarihi { get; set; } = DateTime.Now;
     }
 }
