4e4fe397b5c907051decd0b9136ff50161aaff28
dilayuluerguven <uluerguven245@gmail.com>
2026-01-16 15:41:41 +0300

    Update AlanAnalizi service to send geometry data and handle new API structure

--- realestatemanagementfrontend/src/app/core/alan-analizi/services/alan-analizi.service.ts
+++ realestatemanagementfrontend/src/app/core/alan-analizi/services/alan-analizi.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlanAnaliziService {
 
@@ -12,23 +12,19 @@ export class AlanAnaliziService {
 
   constructor(private http: HttpClient) {}
 
-  geometriKaydet(kullaniciId: number, body: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/geometri-kaydet?kullaniciId=${kullaniciId}`, body);
+  geometriKaydet(body: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/geometri-kaydet`, body);
   }
 
-  kayitliGeometriler(kullaniciId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/geometriler?kullaniciId=${kullaniciId}`);
+  kesisim(a: string, b: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/kesisim`, { a, b });
   }
 
-  kesisim(kullaniciId: number, body: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/kesisim?kullaniciId=${kullaniciId}`, body);
+  birlesimAB(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/birlesim-ab`, {});
   }
 
-  birlesim(kullaniciId: number, body: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/birlesim?kullaniciId=${kullaniciId}`, body);
-  }
-
-  tumAnalizler(kullaniciId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/tum-analizler?kullaniciId=${kullaniciId}`);
+  birlesimABC(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/birlesim-abc`, {});
   }
 }
