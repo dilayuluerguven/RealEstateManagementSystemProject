b916fdd3d5d3e12ce9458507960b65f9c88ca31d
dilayuluerguven <uluerguven245@gmail.com>
2026-01-16 11:08:14 +0300

    Added area analysis entity and initial database migration

--- RealEstateManagementProject/Migrations/20260116080557_AlanAnalizInitial.Designer.cs
+++ RealEstateManagementProject/Migrations/20260116080557_AlanAnalizInitial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RealEstateManagementProject.DataAccess;
@@ -11,9 +12,11 @@ using RealEstateManagementProject.DataAccess;
 namespace RealEstateManagementProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260116080557_AlanAnalizInitial")]
+    partial class AlanAnalizInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
