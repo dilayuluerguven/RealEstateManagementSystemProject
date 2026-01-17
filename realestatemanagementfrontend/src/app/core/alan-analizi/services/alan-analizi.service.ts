ce33a2d5ac453a9731e46ce388248da734f2f449
dilayuluerguven <uluerguven245@gmail.com>
2026-01-17 19:12:32 +0300

    Refactor area analysis service methods

--- realestatemanagementfrontend/src/app/core/alan-analizi/services/alan-analizi.service.ts
+++ realestatemanagementfrontend/src/app/core/alan-analizi/services/alan-analizi.service.ts
@@ -1,48 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ApiResponse, AlanAnalizSonuc } from '../models/alan-analiz-sonuc';
+import { AlanAnalizCreate } from '../models/alan-analiz-create';
+import { AlanAnalizSonuc, ApiResponse } from '../models/alan-analiz-sonuc';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlanAnalizService {
 
-  private apiUrl = 'https://localhost:7275/api/AlanAnaliz';
+  private readonly apiUrl = 'https://localhost:7275/api/alananaliz';
 
   constructor(private http: HttpClient) {}
 
-  geometriKaydet(dto: {
-    geometriAdi: string;
-    geometriJson: string;
-  }): Observable<ApiResponse<AlanAnalizSonuc>> {
-    return this.http.post<ApiResponse<AlanAnalizSonuc>>(
-      `${this.apiUrl}/geometri-kaydet`,
-      dto
-    );
+  kaydet(dto: AlanAnalizCreate): Observable<ApiResponse<AlanAnalizSonuc>> {
+    return this.http.post<ApiResponse<AlanAnalizSonuc>>(`${this.apiUrl}/kaydet`, dto);
   }
 
-  kesisim(dto: {
-    a: string;
-    b: string;
-  }): Observable<ApiResponse<any>> {
-    return this.http.post<ApiResponse<any>>(
-      `${this.apiUrl}/kesisim`,
-      dto
-    );
+  getir(geometriAdi: string): Observable<ApiResponse<AlanAnalizSonuc>> {
+    return this.http.get<ApiResponse<AlanAnalizSonuc>>(`${this.apiUrl}/getir/${geometriAdi}`);
   }
 
-  birlesimAB(): Observable<ApiResponse<AlanAnalizSonuc>> {
-    return this.http.post<ApiResponse<AlanAnalizSonuc>>(
-      `${this.apiUrl}/birlesim-ab`,
-      {}
-    );
+  liste(): Observable<ApiResponse<AlanAnalizSonuc[]>> {
+    return this.http.get<ApiResponse<AlanAnalizSonuc[]>>(`${this.apiUrl}/liste`);
   }
 
-  birlesimABC(): Observable<ApiResponse<AlanAnalizSonuc>> {
-    return this.http.post<ApiResponse<AlanAnalizSonuc>>(
-      `${this.apiUrl}/birlesim-abc`,
-      {}
-    );
+  sil(id: number): Observable<ApiResponse<boolean>> {
+    return this.http.delete<ApiResponse<boolean>>(`${this.apiUrl}/sil/${id}`);
+  }
+
+  temizle(geometriAdi: string): Observable<ApiResponse<boolean>> {
+    return this.http.delete<ApiResponse<boolean>>(`${this.apiUrl}/temizle/${geometriAdi}`);
   }
 }
