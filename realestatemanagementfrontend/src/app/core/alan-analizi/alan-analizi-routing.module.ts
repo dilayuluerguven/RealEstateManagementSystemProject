216b25de5915554123bbf4b487ec86fed426c2e7
dilayuluerguven <uluerguven245@gmail.com>
2026-01-17 17:09:48 +0300

    Update routing configuration for AlanAnalizi feature module

--- realestatemanagementfrontend/src/app/core/alan-analizi/alan-analizi-routing.module.ts
+++ realestatemanagementfrontend/src/app/core/alan-analizi/alan-analizi-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AlanAnaliziComponent } from './components/alan-analizi/alan-analizi.component';
+import { AlanAnalizComponent } from './components/alan-analizi/alan-analizi.component';
+
+
 
 const routes: Routes = [
-  { path: '', component: AlanAnaliziComponent }
+  { path: '', component:AlanAnalizComponent }
 ];
 
 @NgModule({
