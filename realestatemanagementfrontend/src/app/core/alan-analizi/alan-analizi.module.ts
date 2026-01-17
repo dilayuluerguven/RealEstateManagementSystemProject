1054ebcddfd8f2d23601dff58872b18fecfd150f
dilayuluerguven <uluerguven245@gmail.com>
2026-01-17 17:09:55 +0300

    Configure AlanAnalizi module declarations and imports

--- realestatemanagementfrontend/src/app/core/alan-analizi/alan-analizi.module.ts
+++ realestatemanagementfrontend/src/app/core/alan-analizi/alan-analizi.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AlanAnaliziRoutingModule } from './alan-analizi-routing.module';
-import { AlanAnaliziComponent } from './components/alan-analizi/alan-analizi.component';
+import { AlanAnalizComponent } from './components/alan-analizi/alan-analizi.component';
 import { FormsModule } from '@angular/forms';
-
 
 @NgModule({
   declarations: [
-    AlanAnaliziComponent
+    AlanAnalizComponent
   ],
   imports: [
     CommonModule,
