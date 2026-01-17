6bd1786a01fbb3c108fdae5b93b238ab7fa28b17
dilayuluerguven <uluerguven245@gmail.com>
2026-01-17 17:08:59 +0300

    Update AlanAnaliz service interface to include new geometry save method

--- RealEstateManagementProject/Business/Abstract/IAlanAnalizService.cs
+++ RealEstateManagementProject/Business/Abstract/IAlanAnalizService.cs
@@ -1,16 +1,15 @@
 ﻿using NetTopologySuite.Geometries;
 using RealEstateManagementProject.Dtos;
-using RealEstateManagementProject.Entities;
 
 public interface IAlanAnalizService
 {
-    Task<AlanAnaliz> KaydetAsync(int userId, AlanAnalizCreateDto dto);
+    Task<AlanAnalizSonucDto> KaydetAsync(int userId, AlanAnalizCreateDto dto);
 
     Task<Geometry?> GetGeometryAsync(int userId, string geometriAdi);
 
     Task<Geometry?> KesisimAsync(int userId, string a, string b);
 
-    Task<AlanAnaliz> BirlesimABAsync(int userId);
+    Task<AlanAnalizSonucDto> BirlesimABAsync(int userId);
 
-    Task<AlanAnaliz> BirlesimABCAsync(int userId);
+    Task<AlanAnalizSonucDto> BirlesimABCAsync(int userId);
 }
