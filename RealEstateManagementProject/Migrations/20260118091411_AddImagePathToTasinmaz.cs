c6e98e15a97ecde05881783d7f9517e58faea1e5
dilayuluerguven <uluerguven245@gmail.com>
2026-01-19 09:47:50 +0300

    Added new database migration for updated Tasinmaz model

---
+++ RealEstateManagementProject/Migrations/20260118091411_AddImagePathToTasinmaz.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateManagementProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagePathToTasinmaz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Tasinmazlar",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Tasinmazlar");
+        }
+    }
+}
