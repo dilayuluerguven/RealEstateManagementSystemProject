37d4f0d3b5927f4f4e1d68476a853c68398dee25
dilayuluerguven <uluerguven245@gmail.com>
2026-01-19 11:02:09 +0300

    Updated real estate module imports to support search input binding

--- realestatemanagementfrontend/src/app/core/tasinmaz/tasinmaz.module.ts
+++ realestatemanagementfrontend/src/app/core/tasinmaz/tasinmaz.module.ts
@@ -5,7 +5,7 @@ import { TasinmazRoutingModule } from './tasinmaz-routing.module';
 import { AddComponent } from './add/add.component';
 import { ListComponent } from './list/list.component';
 import { UpdateComponent } from './update/update.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CoreModule } from '../core.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
@@ -18,7 +18,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
   ],
   imports: [
     CommonModule,
-    TasinmazRoutingModule,ReactiveFormsModule,SharedModule
+    TasinmazRoutingModule,ReactiveFormsModule,SharedModule,FormsModule
   ]
 })
 export class TasinmazModule { }
