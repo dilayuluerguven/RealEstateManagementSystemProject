931233a8ac0e7bb9c073fdda15184ab7e7f75097
dilayuluerguven <uluerguven245@gmail.com>
2026-01-21 14:08:48 +0300

    Update shared module exports and prepare structure for new components

--- realestatemanagementfrontend/src/app/shared/shared.module.ts
+++ realestatemanagementfrontend/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { TasinmazMapComponent } from './tasinmaz-map/tasinmaz-map.component';
 import { TasinmazListMapComponent } from './tasinmaz-list-map/tasinmaz-list-map.component';
 import { FormsModule } from '@angular/forms';
 import { MapControlsComponent } from './components/map-controls/map-controls/map-controls.component';
+import { ImageUploaderComponent } from './components/image-uploader/image-uploader.component';
 
 
 
@@ -11,14 +12,16 @@ import { MapControlsComponent } from './components/map-controls/map-controls/map
   declarations: [
     TasinmazMapComponent,
     TasinmazListMapComponent,
-    MapControlsComponent
+    MapControlsComponent,
+    ImageUploaderComponent
   ],
   imports: [
     CommonModule,FormsModule
   ],
   exports: [
     TasinmazMapComponent,
-    TasinmazListMapComponent
+    TasinmazListMapComponent,
+    ImageUploaderComponent
   ]
 })
 export class SharedModule {}
